a4b6b95621fdbf71aa2cb4c84150f50ac71d033d
rodrg <rodrgsassuncao@gmail.com>
2024-11-27 21:27:28 +0000

    tela inicial editext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,43 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        // Recuperar componentes EditText
+        // Criar uma variavel e associar (=) o componente de UI <EditText>
+
+
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edit_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_Calcular)
+
+
+
+        btnCalcular.setOnClickListener {
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+
+
+            println("RODRIGO ASSUNÇÃO" + altura)
+        }
+
+
+        }
+
+
+
+
+
+
+
+
 }
