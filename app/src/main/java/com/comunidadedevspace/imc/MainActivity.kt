60cd5ae73a9a5443ad9097001ac8ecafa50008ea
rodrg <rodrgsassuncao@gmail.com>
2024-11-27 21:45:08 +0000

    tela inicial editext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -23,12 +23,17 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
         btnCalcular.setOnClickListener {
-            val peso = edtPeso.text
-            val altura = edtAltura.text
+            val peso: Float = edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
 
 
-            println("RODRIGO ASSUNÇÃO" + altura)
+            println("RODRIGO ASSUNÇÃO" + resultado)
         }
 
 
